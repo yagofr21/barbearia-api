71b449c1a00f62f6b0ba2aa0b766598928e23ff3
Yago Freitas <yagofr21@gmail.com>
2026-07-08 17:34:33 -0300

    Status:final dia 1

--- src/main/java/com/barbearia/barbearia_api/controller/HelloController.java
+++ src/main/java/com/barbearia/barbearia_api/controller/HelloController.java
@@ -1,4 +1,11 @@
-package controller;
+package com.barbearia.barbearia_api.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Olá! Nossa API da Barbearia está funcionando!";
+    }
 }
