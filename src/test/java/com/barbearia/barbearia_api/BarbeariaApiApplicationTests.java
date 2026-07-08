2ee9071bff84a6b88560d78dfa77ad9fce54474d
Yago Freitas <yagofr21@gmail.com>
2026-07-08 15:40:09 -0300

    update:Firts project

---
+++ src/test/java/com/barbearia/barbearia_api/BarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbearia.barbearia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
